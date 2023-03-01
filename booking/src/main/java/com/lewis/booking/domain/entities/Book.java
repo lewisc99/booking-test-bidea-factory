be50a1643026fbf03e38cb787d5796c5f51603c1
lewisc99 <lewiscontato99@gmail.com>
2023-02-28 21:43:34 -0300

    backend - created entity Book

---
+++ booking/src/main/java/com/lewis/booking/domain/entities/Book.java
@@ -0,0 +1,95 @@
+package com.lewis.booking.domain.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "tb_book")
+public class Book {
+
+    @Id
+    private String id;
+    private String name;
+    private String lastName;
+    private Number age;
+    private String phoneNumber;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String houseId;
+    private String discountCode;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Number getAge() {
+        return age;
+    }
+
+    public void setAge(Number age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(String houseId) {
+        this.houseId = houseId;
+    }
+
+    public String getDiscountCode() {
+        return discountCode;
+    }
+
+    public void setDiscountCode(String discountCode) {
+        this.discountCode = discountCode;
+    }
+}

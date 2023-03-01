81e0c66e2ce396e139d346f3dc2646ff2380ff34
lewisc99 <lewiscontato99@gmail.com>
2023-03-01 18:02:03 -0300

    backend - booking - added swagger

--- booking/src/main/java/com/lewis/booking/controller/BookController.java
+++ booking/src/main/java/com/lewis/booking/controller/BookController.java
@@ -10,6 +10,9 @@ import com.lewis.booking.service.contracts.BookService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +30,7 @@ import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping(value = "book")
+@OpenAPIDefinition(info = @Info(title = "Book", version = "1.0.0", description = "SpringBoot Server Dev"))
 public class BookController {
 
     @Autowired
@@ -39,6 +43,10 @@ public class BookController {
     @CircuitBreaker(name = "sample-api", fallbackMethod = "serverDiscountUnavailable")
     @RateLimiter(name="default")
     @Retry(name = "sample-api")
+    @ApiOperation(value="create a new Booking",
+            notes = "will create a new Booking, you must add valid properties",
+            response = BookResponse.class, code = 200
+    )
     public ResponseEntity<BookResponse> create(@RequestBody @Valid BookRequest bookRequest)  {
         Book book =  convertTo.Book(bookRequest);
         BookResponse result = bookService.create(book);

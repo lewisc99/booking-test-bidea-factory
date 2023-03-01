d43f7b655be941b3921b2de2910ea4ce9c570a65
lewisc99 <lewiscontato99@gmail.com>
2023-03-01 16:22:04 -0300

    backend - added eureka client to discount and booking

--- discount/src/main/java/com/lewis/discount/DiscountApplication.java
+++ discount/src/main/java/com/lewis/discount/DiscountApplication.java
@@ -2,8 +2,9 @@ package com.lewis.discount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
+@EnableEurekaClient
 public class DiscountApplication {
 
 	public static void main(String[] args) {
